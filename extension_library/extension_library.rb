aaef1f9d93b3168f1053100c6b6ae4d535ee9bee
Fofiko <foteinid@gmail>
2018-08-07 08:20:06 +0100

    Initialize and setup books

---
+++ extension_library/extension_library.rb
@@ -0,0 +1,17 @@
+class Library
+
+  # attr_reader(:title, :student_name, :date)
+  # attr_writer(:title, :student_name, :date)
+
+
+  def initialize(title, student_name, date)
+    @book = [
+      @title = title
+      @student_name = student_name
+      @date = date
+    ]
+  end
+
+
+
+end
--- extension_library/specs/extension_library_spec.rb
+++ extension_library/specs/extension_library_spec.rb
@@ -2,7 +2,30 @@ require("minitest/autorun")
 require("minitest/rg")
 require_relative("../extension_library.rb")
 
-class Library < Minitest::Test
+class Library < MiniTest::Test
+
+  def setup
+
+    @books = [
+
+      {
+        title: "a tale of two cities",
+        rental_details: {
+          student_name: "Rachel",
+          date: "01/08/18"
+        }
+      },
+
+      {
+        title: "alice_in_wonderland",
+        rental_details: {
+          student_name: "Sian",
+          date: "21/07/18"
+        }
+      }
+    ]
+
+  end
 
 
 
